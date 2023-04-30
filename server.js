b9c10b075c2c0e4863bf0184e0e4585ee474fe76
inwoo2ya <dlsdn92@naver.com>
2023-04-30 15:07:38 +0900

    [2023.04.30][이인우][feat] 코딩애플 nodeProject 73.33%

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const timeTable = () => {
   const todate = new Date();
-  const today = `${todate.getFullYear()}-${("0" + todate.getMonth() + 1).slice(
-    -2
-  )}-${todate.getDate()}`;
+  const today = `${todate.getFullYear()}-${
+    "0" + (todate.getMonth() + 1)
+  }-${todate.getDate()}`;
   return today;
 };
+require("dotenv").config();
 setTimeout(timeTable, 1000);
 const express = require("express");
 const app = express();
+const bcrypt = require("bcrypt");
+
+const hashedPassword = async password => {
+  return bcrypt.hashSync(password, 10);
+};
 const bodyParser = require("body-parser");
 // body-parser는 요청 데이터(body) 해석을 쉽게 도와줌
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,23 +22,20 @@ app.use(methodOverride("_method"));
 const MongoClient = require("mongodb").MongoClient;
 app.set("view engine", "ejs");
 let db;
-MongoClient.connect(
-  "mongodb+srv://admin:n37w0rk@project0.ebrquly.mongodb.net/?retryWrites=true&w=majority",
-  (err, client) => {
-    if (err) {
-      console.log(err);
-    } else {
-      db = client.db("nodeapp"); //연결할 db이름 저장
+MongoClient.connect(process.env.DB_URL, (err, client) => {
+  if (err) {
+    console.log(err);
+  } else {
+    db = client.db("nodeapp"); //연결할 db이름 저장
 
-      // db.collection('data1').insertOne({Name:'inwoo',age:24},(err,res)=>{
-      //     console.log('저장완료');
-      // });
-      app.listen(8080, () => {
-        console.log("listening on 8080");
-      }); // 서버띄울 포트번호, 띄운 후 실행할 코드
-    }
+    // db.collection('data1').insertOne({Name:'inwoo',age:24},(err,res)=>{
+    //     console.log('저장완료');
+    // });
+    app.listen(process.env.PORT, () => {
+      console.log("listening on 8080");
+    }); // 서버띄울 포트번호, 띄운 후 실행할 코드
   }
-);
+});
 
 // 위에는 서버를 띄우기 위한 기본 셋팅
 
@@ -44,67 +47,8 @@ app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
-app.get("/write", (req, res) => {
-  res.render("write.ejs");
-});
-
-app.post("/add", (req, res) => {
-  res.render("write.ejs");
-  db.collection("counter").findOne({ name: "게시물 갯수" }, (err, res) => {
-    console.log(res.totalPost);
-    let totalPostNum = res.totalPost;
-    db.collection("data1").insertOne(
-      {
-        _id: totalPostNum + 1,
-        name: req.body.name,
-        date: timeTable(),
-        content: req.body.content,
-        file: req.body.file,
-      },
-      (err, result) => {
-        if (err) {
-          console.error(err);
-        } else {
-          console.log("저장 완료");
-          //counter 라는 컬렉션에 있는 totalPost 라는 항목도 1증가 시켜야함
-          db.collection("counter").updateOne(
-            { name: "게시물 갯수" },
-            { $inc: { totalPost: 1 } }, // $inc:{ field명: 증가 값 } => 값 증가 , $set:{field명: 세팅 값} => 값 세팅
-            (err, res) => {
-              if (err) {
-                return console.log(err);
-              }
-            }
-          );
-        }
-      }
-    );
-  });
-});
 //list 로 get요청으로 접속하면
 // 실제 DB에 저장된 데이터들로 예쁘게 꾸며진 HTML을 보여줌
-app.get("/list", (req, res) => {
-  //DB에 저장된 data1라는 collection안의 모든 데이터를 꺼내주세요
-  db.collection("data1")
-    .find()
-    .toArray((err, result) => {
-      console.log(result);
-      res.render("list.ejs", { datas: result });
-    });
-});
-
-app.delete("/delete", (req, res) => {
-  console.log(req.body);
-  req.body._id = parseInt(req.body._id);
-  db.collection("data1").deleteOne(req.body, (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("삭제완료");
-      res.status(200).send({ message: "성공" });
-    }
-  });
-});
 
 app.get("/edit/:id", (req, res) => {
   db.collection("data1").findOne(
@@ -122,7 +66,7 @@ app.put("/edit", (req, res) => {
     // 수정 값
     {
       $set: {
-        name: req.body.name,
+        title: req.body.title,
         date: timeTable(),
         content: req.body.content,
         file: req.body.file,
@@ -143,6 +87,7 @@ app.put("/edit", (req, res) => {
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const session = require("express-session");
+const { ObjectID } = require("bson");
 
 app.use(
   session({
@@ -201,7 +146,7 @@ passport.serializeUser((user, done) => {
 passport.deserializeUser((id, done) => {
   // 로그인된 유저가 마이페이지 접속했을 때 실행되는 함수(myPage 접속시 발동)
   // 로그인한 유저의 세션아이디를 바탕으로 개인정보를 DB에서 찾는 역할
-  db.collection("user").findOne({ id: id }, (err, res) => {
+  db.collection("user").findOne({ id }, (err, res) => {
     done(null, res);
   });
 });
@@ -220,9 +165,122 @@ function loginCheck(req, res, next) {
 app.get("/mypage", loginCheck, (req, res) => {
   res.render("myPage.ejs", { user: req.user });
 });
+app.get("/write", loginCheck, (req, res) => {
+  res.render("write.ejs", { user: req.user });
+});
+
+app.get("/list", loginCheck, (req, res) => {
+  //DB에 저장된 data1라는 collection안의 모든 데이터를 꺼내주세요
+  db.collection("data1")
+    .find()
+    .toArray((err, result) => {
+      console.log(result);
+      res.render("list.ejs", { datas: result, user: req.user });
+    });
+});
 
 app.get("/signup", (req, res) => {
   res.render("signup.ejs");
 });
 
-app.post("/signup", (req, res) => {});
+app.post("/signup", (req, res) => {
+  if (req.body.user_pw != req.body.confirm_pw) {
+    res.send("<script> alert('비밀번호가 틀렸어요');</script>");
+  } else {
+    // db.collection("user").findOne({
+    //   id: req.body.user_id,
+    // }),
+    //   (err, result) => {
+    // if (result) {
+    //   return res
+    //     .status(400)
+    //     .json({ msg: "이미 같은 아이디가 존재합니다." });
+    // }
+    db.collection("user").findOne({ id: req.body.user_id }, (res, err) => {
+      if (!res) {
+        db.collection("user").insertOne(
+          {
+            _id: ObjectID,
+            id: req.body.user_id,
+            pw: req.body.user_pw,
+            // pw: hashedPassword(req.body.user_pw),
+          },
+          (err, result) => {
+            if (err) {
+              console.log(err);
+            } else {
+              res.redirect("/login");
+            }
+          }
+        );
+      }
+    });
+  }
+});
+app.post("/add", (req, res) => {
+  res.render("write.ejs");
+  db.collection("counter").findOne({ name: "게시물 갯수" }, (err, res) => {
+    console.log(res.totalPost);
+    let totalPostNum = res.totalPost;
+    let createPost = {
+      _id: totalPostNum + 1,
+      title: req.body.title,
+      date: timeTable(),
+      content: req.body.content,
+      file: req.body.file,
+      writerId: req.user._id,
+    };
+    db.collection("data1").insertOne(createPost, (err, result) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log("저장 완료");
+        //counter 라는 컬렉션에 있는 totalPost 라는 항목도 1증가 시켜야함
+        db.collection("counter").updateOne(
+          { name: "게시물 갯수" },
+          { $inc: { totalPost: 1 } }, // $inc:{ field명: 증가 값 } => 값 증가 , $set:{field명: 세팅 값} => 값 세팅
+          (err, res) => {
+            if (err) {
+              return console.log(err);
+            }
+          }
+        );
+      }
+    });
+  });
+});
+app.delete("/delete", (req, res) => {
+  console.log(req.body);
+  req.body._id = parseInt(req.body._id);
+  let deleteData = { _id: req.body._id, writerId: req.user._id };
+  db.collection("data1").deleteOne(deleteData, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("삭제완료");
+      res.status(200).send({ message: "성공" });
+    }
+  });
+});
+app.get("/search", (req, res) => {
+  let searchCondition = [
+    {
+      $search: {
+        index: "titleSearch",
+        text: {
+          query: req.query.value,
+          path: ["title", "content"],
+        },
+      },
+    },
+    { $sort: { _id: 1 } }, //정렬 1: 내림차순, -1: 오름차순
+    // { $limit: 10} // 10개만 가져와라
+    // { $project: { title: 1, _id: 0, score: { $meta: "searchScore" } } }, 검색결과 필터 주기
+  ];
+  db.collection("data1")
+    .aggregate(searchCondition) // 검색 조건을 달 수 있는 find data 파이프라인 구축
+    .toArray((err, result) => {
+      console.log(result);
+      res.render("search.ejs", { datas: result, result: req.query.value });
+    });
+});
